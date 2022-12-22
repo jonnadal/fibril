b2c10970470ccfe0389d222a0b8afae04ded4cfe
Christian Legnitto <christian@legnitto.com>
2022-12-21 21:05:19 -0400

    Add `tracing` support.
    Fixes https://github.com/jonnadal/fibril/issues/2.

--- fiber/src/fiber.rs
+++ fiber/src/fiber.rs
@@ -4,22 +4,49 @@ use {
     fibril_core::{Command, Event, Step},
 };
 
+#[cfg(feature = "tracing")]
+use tracing::{event, span, Level};
+
 impl<'a, M> Fiber<'a, M> {
     pub fn new(behavior: impl FnOnce(Sdk<'_, M>) + 'a) -> Self {
-        Self(ScopedCoroutine::new(move |yielder, spawn_ok| {
-            let id = match spawn_ok {
-                Event::SpawnOk(id) => id,
-                _ => unreachable!(),
-            };
-            behavior(Sdk(yielder, id))
-        }))
+        #[cfg(feature = "tracing")]
+        let span = span!(Level::TRACE, "fiber");
+
+        Self(
+            ScopedCoroutine::new(move |yielder, spawn_ok| {
+                let id = match spawn_ok {
+                    Event::SpawnOk(id) => id,
+                    _ => unreachable!(),
+                };
+
+                #[cfg(feature = "tracing")]
+                event!(Level::TRACE, "fiber id: {}", id);
+
+                behavior(Sdk(yielder, id))
+            }),
+            #[cfg(feature = "tracing")]
+            span,
+        )
     }
 }
 impl<M> Step<M> for Fiber<'_, M> {
     fn step(&mut self, event: Event<M>) -> Command<M> {
+        #[cfg(feature = "tracing")]
+        let span = span!(Level::TRACE, "step");
+        #[cfg(feature = "tracing")]
+        let _enter = span.enter();
+
         match self.0.resume(event) {
-            CoroutineResult::Yield(command) => command,
-            CoroutineResult::Return(()) => Command::Exit,
+            CoroutineResult::Yield(command) => {
+                #[cfg(feature = "tracing")]
+                event!(Level::TRACE, "yield");
+                command
+            }
+            CoroutineResult::Return(()) => {
+                #[cfg(feature = "tracing")]
+                event!(Level::TRACE, "return");
+                Command::Exit
+            }
         }
     }
 }
