51a7fa1e244c9b18e5e1946ff6ee9d68b6b23e08
Jonathan Nadal <jon.nadal@gmail.com>
2023-01-08 08:02:06 -0800

    improve an example

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -4,7 +4,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! fibril = { version = "0", features = ["serde"] }
+//! fibril = { version = "0", features = ["serde_json", "rt"] }
 //! serde = { version = "1", features = ["derive"] }
 //!
 //! [dev-dependencies]
@@ -13,42 +13,78 @@
 //!
 //! ```rust
 //! use fibril::*;
-//! use fibril_verifier::*;
 //!
 //! #[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 //! enum Msg { Inc, Double, Value(u64) }
 //!
-//! let mut verifier = Verifier::new(|cfg| {
-//!     let server = cfg.spawn(Fiber::new(|sdk| {
-//!         let mut val = 0;
-//!         loop {
-//!             let (src, msg) = sdk.recv();
-//!             match msg {
-//!                 Msg::Inc => {
-//!                     val += 1;
-//!                     sdk.send(src, Msg::Value(val));
-//!                 }
-//!                 Msg::Double => {
-//!                     val *= 2;
-//!                     sdk.send(src, Msg::Value(val));
-//!                 }
-//!                 _ => sdk.exit(),
+//! fn server(sdk: Sdk<Msg>) {
+//!     let mut val = 0;
+//!     loop {
+//!         let (src, msg) = sdk.recv();
+//!         match msg {
+//!             Msg::Inc => {
+//!                 val += 1;
+//!                 sdk.send(src, Msg::Value(val));
 //!             }
+//!             Msg::Double => {
+//!                 val *= 2;
+//!                 sdk.send(src, Msg::Value(val));
+//!             }
+//!             _ => sdk.exit(),
 //!         }
-//!     }));
-//!     cfg.spawn(Fiber::new(move |sdk| {
-//!         sdk.send(server, Msg::Inc);
-//!         let (_src, msg) = sdk.recv();
-//!         assert_eq!(msg, Msg::Value(1)); // truth depends on race winner
-//!     }));
-//!     cfg.spawn(Fiber::new(move |sdk| {
-//!         sdk.send(server, Msg::Double);
-//!         let (_src, msg) = sdk.recv();
-//!         assert_eq!(msg, Msg::Value(2)); // truth depends on race winner
-//!     }));
-//! });
-//! let (msg, _minimal_trace) = verifier.assert_panic();
-//! assert!(msg.contains("left == right"));
+//!     }
+//! }
+//!
+//! // In the binary, bind the server to a UDP socket.
+//! fn main() {
+//!     use std::net::Ipv4Addr;
+//!     let mut rt = UdpRuntime::new_with_serde_json()
+//!         .ipv4(Ipv4Addr::LOCALHOST)
+//!         .port_fn(|n| 3000 + n);
+//!     rt.spawn(|| Fiber::new(server));
+//!     rt.join().unwrap();
+//! }
+//!
+//! // In the tests, the model checker will attempt to find a "trace" (sequence of steps) that
+//! // causes a panic.
+//! #[cfg(test)]
+//! #[test]
+//! # fn hack_so_next_fn_is_compiled_in_rustdoc() {}
+//! fn may_panic() {
+//!     use fibril_verifier::*;
+//!     let mut verifier = Verifier::new(|cfg| {
+//!         let server = cfg.spawn(Fiber::new(server));
+//!         cfg.spawn(Fiber::new(move |sdk| {
+//!             sdk.send(server, Msg::Inc);
+//!             let (_src, msg) = sdk.recv();
+//!             assert_eq!(msg, Msg::Value(1)); // truth depends on race winner
+//!         }));
+//!         cfg.spawn(Fiber::new(move |sdk| {
+//!             sdk.send(server, Msg::Double);
+//!             let (_src, msg) = sdk.recv();
+//!             assert_eq!(msg, Msg::Value(2)); // truth depends on race winner
+//!         }));
+//!     });
+//!     // TIP: alternatively use verifier.assert_no_panic() to fail the test.
+//!     let (msg, _minimal_trace) = verifier.assert_panic();
+//!     assert!(msg.contains("left == right"));
+//! }
+//! # may_panic();
+//! ```
+//!
+//! And here is an example of how to interact with the server using
+//! [netcat](https://en.wikipedia.org/wiki/Netcat).
+//!
+//! ```sh
+//! # send STDOUT to disk for clarity as replies are not newline delimited
+//! $ nc -u 127.0.0.1 3000 > /tmp/replies
+//! "Inc"
+//! "Inc"
+//! "Double"
+//! ^C
+//!
+//! $ cat /tmp/replies
+//! {"Value":1}{"Value":2}{"Value":4}
 //! ```
 
 #![deny(unused_must_use)]
