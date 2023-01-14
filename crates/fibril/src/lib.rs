4503188e9b2de13fd345bd02b1d85160519541f1
Jonathan Nadal <jon.nadal@gmail.com>
2023-01-14 05:34:28 -0800

    enable doc_auto_cfg

--- crates/fibril/src/lib.rs
+++ crates/fibril/src/lib.rs
@@ -1,5 +1,7 @@
 //! Fibril is a library for implementing distributed systems with commmunicating fibers.
 
+#![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
+
 #![deny(unused_must_use)]
 #![warn(rust_2018_idioms, unreachable_pub)]
 
